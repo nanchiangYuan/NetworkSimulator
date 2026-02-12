87b6b963f47911cff374a716a27e836281c26c45
Ashley Yuan <ashleyyuan.work@gmail.com>
2026-02-11 22:36:06 -0600

    added methods that helps with implementing a single timer for timeouts

--- Scheduler.java
+++ Scheduler.java
@@ -4,11 +4,12 @@ public class Scheduler {
 
     private double currentTime; // in ms
     private PriorityQueue<Event> global_queue;
+    private Event timer;
 
     Scheduler (){   
         this.currentTime = 0;
         this.global_queue = new PriorityQueue<>();
-
+        this.timer = null;
     }
 
     public double getCurrentTime() {
@@ -27,9 +28,31 @@ public class Scheduler {
         this.global_queue.add(e);
     }
 
-    public Event runSchedule() {
+    public Event run() {
         Event tobeRun = this.global_queue.poll();
         this.currentTime = tobeRun.getTime();
         return tobeRun;
+    }
+
+    public Event getTimer() {
+        return timer;
+    }
+
+    public void setTimer(int oldestSeqNo, double timeout) {
+        if(timer != null)
+            global_queue.remove(timer);
+        timer = new Event(oldestSeqNo, Event.EventType.TIMEOUT_CHECK, currentTime + timeout);
+        global_queue.add(timer);
+    }
+
+    public void print() {
+        System.out.println("     ======= current schedule: ");
+        for(Event e: global_queue) {
+            if(e.getType() == Event.EventType.TIMEOUT_CHECK) 
+                System.out.println("       " + e.toTimeoutString());
+            else
+                System.out.println("       " + e);
+        }
+        System.out.println("     ==========================");
     }
 }
